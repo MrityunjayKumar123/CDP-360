d778157adf40bdde9323c77190a1b0b2d3f0ce90
Mrityunjay Kumar <43465754+MrityunjayKumar123@users.noreply.github.com>
2026-06-23 14:48:59 +0530

    Refactor streaming job to write to PostgreSQLNEw
    Removed Bronze to Delta Lake write stream and added PostgreSQL write functionality.

--- cdp_project/processing/spark_streaming_job.py
+++ cdp_project/processing/spark_streaming_job.py
@@ -1,10 +1,19 @@
 # processing/spark_streaming_job.py
 
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
+hadoop_home = "D:/MJ/CDP-360-main/CDP-360-main/cdp_project"
+os.environ["HADOOP_HOME"] = hadoop_home
+os.environ["PATH"] = f"{hadoop_home}/bin;{os.environ.get('PATH', '')}"
+
 spark = SparkSession.builder \
     .appName("CDPStreaming") \
+    .master("local[*]") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.1.2") \
+    .config("spark.jars.excludes", "com.google.code.findbugs:jsr305") \
+    .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", "true") \
     .getOrCreate()
 
 # Read from Kafka
@@ -18,14 +27,7 @@ parsed = df.selectExpr("CAST(value AS STRING) as json") \
     .selectExpr("from_json(json, 'loan_id STRING, customer_id STRING, status STRING, timestamp STRING') as data") \
     .select("data.*")
 
-# Write Bronze → Delta Lake
-parsed.writeStream.format("delta") \
-    .option("checkpointLocation", "/tmp/delta/checkpoints/bronze") \
-    .option("path", "/tmp/delta/bronze") \
-    .outputMode("append") \
-    .start()
-
-# Simultaneous write to PostgreSQL (operational fields)
+# Write to PostgreSQL
 parsed.writeStream \
     .foreachBatch(lambda batch_df, _: batch_df.write \
         .format("jdbc") \
